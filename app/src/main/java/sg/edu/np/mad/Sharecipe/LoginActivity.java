4e1bcb066510f2401cc5b42ae271c7719a070d2d
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-05-22 13:18:59 +0800

    testing http url builder.

--- app/src/main/java/sg/edu/np/mad/Sharecipe/LoginActivity.java
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/LoginActivity.java
@@ -13,10 +13,9 @@ import org.jetbrains.annotations.NotNull;
 import java.io.IOException;
 
 import java9.util.concurrent.CompletableFuture;
-import java9.util.function.Consumer;
-import java9.util.function.Function;
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -36,8 +35,14 @@ public class LoginActivity extends AppCompatActivity {
         Button signUpNow = findViewById(R.id.buttonSignupNow);
 
         login.setOnClickListener(v -> {
+            HttpUrl url = new HttpUrl.Builder()
+                    .scheme("https")
+                    .host("sharecipe-backend.herokuapp.com")
+                    .addPathSegment("hello")
+                    .build();
+
             Request request = new Request.Builder()
-                    .url("https://sharecipe-backend.herokuapp.com/hello")
+                    .url(url)
                     .get()
                     .build();
 
