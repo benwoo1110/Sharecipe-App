4a421c7acd4cd606d59eee7282d574f970237657
hokkaidomelk <72664507+hokkaidomelk@users.noreply.github.com>
2021-05-30 20:16:37 +0800

    add id for recipe_info, create tab layout and tab items in recipeinfo, created new activity for recipestepslist.

--- app/src/main/java/sg/edu/np/mad/Sharecipe/ui/RecipeInfo.java
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/RecipeInfo.java
@@ -1,8 +1,13 @@
 package sg.edu.np.mad.Sharecipe.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import sg.edu.np.mad.Sharecipe.R;
 
@@ -12,5 +17,22 @@ public class RecipeInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_recipe_info);
+        TabLayout tabLayout = findViewById(R.id.recipeTab);
+        TabItem tabInfo = findViewById(R.id.tabInfo);
+
+//        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+//
+  //          override fun onTabSelected(tab: TabLayout.Tab?) {
+    //            // Handle tab select
+      //      }
+//
+  //          override fun onTabReselected(tab: TabLayout.Tab?) {
+    //            // Handle tab reselect
+      //      }
+//
+  //          override fun onTabUnselected(tab: TabLayout.Tab?) {
+    //            // Handle tab unselect
+      //      }
+        //});
     }
 }
---
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/RecipeStepsList.java
@@ -0,0 +1,13 @@
+package sg.edu.np.mad.Sharecipe.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipeStepsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
