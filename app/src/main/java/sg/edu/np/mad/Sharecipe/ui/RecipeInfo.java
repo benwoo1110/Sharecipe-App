0e9ae630704f2190e38033b311690fa9e2d52c38
hokkaidomelk <72664507+hokkaidomelk@users.noreply.github.com>
2021-05-30 19:06:13 +0800

    created recipe info activity, linked to recipe info layout

---
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/RecipeInfo.java
@@ -0,0 +1,16 @@
+package sg.edu.np.mad.Sharecipe.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import sg.edu.np.mad.Sharecipe.R;
+
+public class RecipeInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_recipe_info);
+    }
+}
