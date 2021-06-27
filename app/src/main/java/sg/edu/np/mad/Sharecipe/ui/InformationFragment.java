0fc99144b9cf65f5b30969caffde65eab5a27340
hokkaidomelk <72664507+hokkaidomelk@users.noreply.github.com>
2021-06-28 00:41:07 +0800

    created recipe information variables

--- app/src/main/java/sg/edu/np/mad/Sharecipe/ui/InformationFragment.java
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/InformationFragment.java
@@ -1,9 +1,15 @@
 package sg.edu.np.mad.Sharecipe.ui;
 
+import android.media.Rating;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,8 +28,22 @@ public class InformationFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_information, container, false);
+        View view = inflater.inflate(R.layout.fragment_information, container, false);
+
+        EditText name = view.findViewById(R.id.nameInput);
+        CheckBox status = view.findViewById(R.id.publicCheck);
+        RatingBar difficulty = view.findViewById(R.id.difficultyInput);
+        EditText portion = view.findViewById(R.id.servesInput);
+        EditText prepTime = view.findViewById(R.id.prepInput);
+        ImageButton image = view.findViewById(R.id.addImage);
+
+        return view;
     }
 
 }
