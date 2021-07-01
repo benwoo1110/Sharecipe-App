a59f7f76404a80c1f4f960ec672278958fce49dd
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-07-01 18:18:08 +0800

    Implement basic confirm password checking.

--- app/src/main/java/sg/edu/np/mad/Sharecipe/ui/RegisterActivity.java
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/RegisterActivity.java
@@ -1,6 +1,7 @@
 package sg.edu.np.mad.Sharecipe.ui;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
@@ -27,8 +28,19 @@ public class RegisterActivity extends DynamicFocusAppCompatActivity {
 
         signUp.setOnClickListener(v -> {
             hideSoftKeyBoard();
+
+            String usernameText = username.getEditText().getText().toString();
+            String bioText = bio.getEditText().getText().toString();
+            String passwordText = password.getEditText().getText().toString();
+            String passwordConfirmText = passwordConfirm.getEditText().getText().toString();
+
+            if (!passwordText.equals(passwordConfirmText)) {
+                passwordConfirm.setError("Password does not match.");
+                return;
+            }
+
             AccountManager.getInstance(this)
-                    .register(username.getEditText().getText().toString(), password.getEditText().getText().toString(), bio.getEditText().getText().toString())
+                    .register(usernameText, passwordText, bioText)
                     .onSuccess(account -> {
                         Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
