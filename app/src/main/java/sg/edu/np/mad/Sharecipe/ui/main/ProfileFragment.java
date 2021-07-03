7c75a6562d59b2c65142b818e1150a8d693e1327
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-07-03 14:10:32 +0800

    Dummy branch change.

--- app/src/main/java/sg/edu/np/mad/Sharecipe/ui/main/ProfileFragment.java
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/main/ProfileFragment.java
@@ -31,8 +31,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-
-
+        
         ImageView profileImage = view.findViewById(R.id.profileImage);
         Button logoutButton = view.findViewById(R.id.buttonLogout);
         TextView username = view.findViewById(R.id.username);
