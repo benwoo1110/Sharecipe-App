105eb4790a85537ec3d188166bd97ed14b0f4fa6
hokkaidomelk <72664507+hokkaidomelk@users.noreply.github.com>
2021-07-04 22:57:57 +0800

    Added recyclerview and other items
    Added recyclerview and its adapter, viewholder as well as new icon for image button.

---
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/create/StepsAdapter.java
@@ -0,0 +1,39 @@
+package sg.edu.np.mad.Sharecipe.ui.create;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import sg.edu.np.mad.Sharecipe.R;
+import sg.edu.np.mad.Sharecipe.models.RecipeStep;
+
+public class StepsAdapter extends RecyclerView.Adapter<StepsViewholder> {
+    ArrayList<RecipeStep> data;
+
+    public StepsAdapter(ArrayList<RecipeStep> input) {this.data = input;}
+
+    public StepsViewholder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_steps, parent, false);
+        return new StepsViewholder(item);
+    }
+
+    public void onBindViewHolder(StepsViewholder holder, int position) {
+        RecipeStep step = data.get(position);
+        holder.stepNumber.setText(step.getStepNumber());
+        holder.stepDescription.setText(step.getDescription());
+        holder.edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public int getItemCount() {
+        return data.size();
+    }
+}
--- app/src/main/java/sg/edu/np/mad/Sharecipe/ui/create/StepsFragment.java
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/create/StepsFragment.java
@@ -19,8 +19,7 @@ public class StepsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_steps, container, false);
-        RecyclerView stepsView;
-        
+        RecyclerView stepsView = view.findViewById(R.id.recyclerview_steps);
 
         return view;
     }
