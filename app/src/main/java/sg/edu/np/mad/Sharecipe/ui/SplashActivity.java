7ffd817ded11de8747bca145e0fabce7115cc29b
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-07-06 22:41:53 +0800

    Show error dialog on connection fail.

--- app/src/main/java/sg/edu/np/mad/Sharecipe/ui/SplashActivity.java
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/SplashActivity.java
@@ -3,6 +3,8 @@ package sg.edu.np.mad.Sharecipe.ui;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -19,9 +21,11 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         setContentView(R.layout.activity_splash);
+        init();
+    }
 
+    private void init() {
         AccountManager.getInstance(this).getOrRefreshAccount().onSuccess(account -> {
             Intent intent = new Intent(SplashActivity.this, MainActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -31,8 +35,20 @@ public class SplashActivity extends AppCompatActivity {
             Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
-        }).onError(error -> {
-            SplashActivity.this.runOnUiThread(() -> Toast.makeText(SplashActivity.this, "Server error ;(", Toast.LENGTH_SHORT).show());
-        });
+        }).onError(error -> SplashActivity.this.runOnUiThread(this::showErrorDialog));
+    }
+
+    private void showErrorDialog() {
+        new AlertDialog.Builder(SplashActivity.this)
+                .setTitle("Whoops!")
+                .setMessage("Server error. Make sure you are connected to the internet.")
+                .setCancelable(false)
+                .setPositiveButton("try again", (dialog, which) -> {
+                    init();
+                })
+                .setNegativeButton("exit", (dialog, which) -> {
+                    finish();
+                })
+                .show();
     }
 }
