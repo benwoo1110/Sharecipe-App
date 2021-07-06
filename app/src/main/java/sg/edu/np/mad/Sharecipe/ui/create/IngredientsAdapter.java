7818ff129b77bacf8e7939eb7aaf3668ad02a617
hokkaidomelk <72664507+hokkaidomelk@users.noreply.github.com>
2021-07-06 22:15:18 +0800

    create adapter and viewholder for ingredients recyclerview

---
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/create/IngredientsAdapter.java
@@ -0,0 +1,35 @@
+package sg.edu.np.mad.Sharecipe.ui.create;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import sg.edu.np.mad.Sharecipe.R;
+import sg.edu.np.mad.Sharecipe.models.Ingredient;
+
+public class IngredientsAdapter extends RecyclerView.Adapter<IngredientsViewholder> {
+    ArrayList<Ingredient> ingredientsList;
+
+    public IngredientsAdapter() {}
+
+    public IngredientsViewholder onCreateViewHolder(ViewGroup parent, int viewtype) {
+        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_ingredients, parent, false);
+        IngredientsViewholder holder = new IngredientsViewholder(item);
+
+        return holder;
+    }
+
+    public void onBindViewHolder(IngredientsViewholder holder, int position) {
+        //TODO: stuff
+    }
+
+    public int getItemCount() {
+        return ingredientsList.size();
+    }
+
+}
+
---
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/create/IngredientsViewholder.java
@@ -0,0 +1,20 @@
+package sg.edu.np.mad.Sharecipe.ui.create;
+
+import android.view.View;
+import android.widget.Spinner;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class IngredientsViewholder extends RecyclerView.ViewHolder {
+    TextInputEditText name;
+    TextInputEditText quantity;
+    Spinner unit;
+
+    public IngredientsViewholder(View itemView) {
+        super(itemView);
+
+    }
+
+}
