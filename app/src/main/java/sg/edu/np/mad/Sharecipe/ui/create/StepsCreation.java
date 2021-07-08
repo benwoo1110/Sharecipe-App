c869f65ff2e6c29e308794a5599aa5db6bc85653
hokkaidomelk <72664507+hokkaidomelk@users.noreply.github.com>
2021-07-09 00:34:07 +0800

    edit steps recyclerview widgets. create new steps creation activity

---
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/create/StepsCreation.java
@@ -0,0 +1,16 @@
+package sg.edu.np.mad.Sharecipe.ui.create;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import sg.edu.np.mad.Sharecipe.R;
+
+public class StepsCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_steps_creation);
+    }
+}
