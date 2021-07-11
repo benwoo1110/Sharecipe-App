b79c90d92ee0ec42d1eb78a131386ec33ea1779d
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-07-12 00:05:10 +0800

    Remove use of FragmentCollection.

--- app/src/main/java/sg/edu/np/mad/Sharecipe/ui/main/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/main/MainActivity.java
@@ -1,34 +1,25 @@
 package sg.edu.np.mad.Sharecipe.ui.main;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import sg.edu.np.mad.Sharecipe.R;
 import sg.edu.np.mad.Sharecipe.ui.common.MenuStateAdapter;
-import sg.edu.np.mad.Sharecipe.ui.create.RecipeCreateActivity;
 import sg.edu.np.mad.Sharecipe.ui.main.discover.DiscoverFragment;
 import sg.edu.np.mad.Sharecipe.ui.main.profile.ProfileFragment;
 import sg.edu.np.mad.Sharecipe.ui.main.recipe.MyRecipeFragment;
 import sg.edu.np.mad.Sharecipe.ui.main.search.SearchFragment;
-import sg.edu.np.mad.Sharecipe.ui.common.FragmentCollection;
 
 public class MainActivity extends AppCompatActivity {
-
-    private FragmentCollection fragmentCollection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        fragmentCollection = new FragmentCollection();
 
         ViewPager2 mainViewPager = findViewById(R.id.mainViewPager);
         BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
@@ -48,7 +39,5 @@ public class MainActivity extends AppCompatActivity {
         });
 
         bottomNavigation.setSelectedItemId(R.id.discover_menu);
-
-
     }
 }
