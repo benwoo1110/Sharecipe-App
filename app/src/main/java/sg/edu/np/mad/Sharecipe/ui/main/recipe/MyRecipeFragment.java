7a79222b7b9e2979455c5c0f998d0a03c9688550
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-07-11 21:41:09 +0800

    Remove unless import.

--- app/src/main/java/sg/edu/np/mad/Sharecipe/ui/main/recipe/MyRecipeFragment.java
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/main/recipe/MyRecipeFragment.java
@@ -1,7 +1,6 @@
 package sg.edu.np.mad.Sharecipe.ui.main.recipe;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
