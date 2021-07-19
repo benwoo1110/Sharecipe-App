3a9f2c67c72198878a1438044a14da89443e9d91
hokkaidomelk <72664507+hokkaidomelk@users.noreply.github.com>
2021-07-19 20:51:17 +0800

    display recipe portions and preparation time

--- app/src/main/java/sg/edu/np/mad/Sharecipe/ui/view/ViewInformationFragment.java
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/view/ViewInformationFragment.java
@@ -30,11 +30,44 @@ public class ViewInformationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_view_information, container, false);
         TextInputEditText displayName = view.findViewById(R.id.viewRecipeName);
+        TextInputEditText displayPortion = view.findViewById(R.id.viewPortion);
+        TextInputEditText displayPrep = view.findViewById(R.id.viewPrep);
 
+        // Get recipe information and store to variables
         String name = recipe.getName();
+        int portions = recipe.getPortion();
+        int prepSeconds = recipe.getTotalTimeNeeded();
+
+        // Set stored variables to android widgets
+        // Set name of recipe
         displayName.setText(name);
+
+        // Set number of portions, display not specified if it is 0
+        if (portions > 0) {
+            displayPortion.setText("Serves " + String.valueOf(portions));
+        }
+        else {
+            displayPortion.setText("not specified");
+        }
+
+        if (prepSeconds < 0) {
+            displayPrep.setText("not specified");
+        }
+
+        else if (prepSeconds < 3600) {
+            int minutes = prepSeconds / 60;
+            displayPrep.setText(String.valueOf(minutes) + " minutes");
+        }
+
+        else {
+            int prepMinutes = prepSeconds / 60;
+            int hours = (prepMinutes - prepMinutes % 60) / 60;
+            int minutes = prepMinutes - (hours * 60);
+            displayPrep.setText(String.valueOf(hours) + " hours " + String.valueOf(minutes) + " minutes");
+        }
 
 
         return view;
     }
+
 }
