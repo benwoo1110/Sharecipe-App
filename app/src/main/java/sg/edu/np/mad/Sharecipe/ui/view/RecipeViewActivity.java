fe0a91810181c31a738c8b2579360766cab61778
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-07-26 11:22:35 +0800

    Toast recipe failed to load.

--- app/src/main/java/sg/edu/np/mad/Sharecipe/ui/view/RecipeViewActivity.java
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/view/RecipeViewActivity.java
@@ -5,6 +5,7 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -47,6 +48,9 @@ public class RecipeViewActivity extends AppCompatActivity {
                 RecipeViewAdapter adapter = new RecipeViewAdapter(this, tabLayout.getTabCount(), recipe);
                 viewpager.setAdapter(adapter);
             });
-        }).onFailed(failed -> finish());
+        }).onFailed(failed -> {
+            Toast.makeText(RecipeViewActivity.this, "Failed to load recipe.", Toast.LENGTH_SHORT).show();
+            finish();
+        });
     }
 }
