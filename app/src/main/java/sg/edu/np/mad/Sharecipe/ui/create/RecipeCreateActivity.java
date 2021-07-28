c05592f03c57481749c4b3dde58ffc6cabc7665a
hokkaidomelk <72664507+hokkaidomelk@users.noreply.github.com>
2021-07-28 21:59:46 +0800

    improve create recipe menu, implement alert dialogs for exiting or saving recipe creation

--- app/src/main/java/sg/edu/np/mad/Sharecipe/ui/create/RecipeCreateActivity.java
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/create/RecipeCreateActivity.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -37,6 +39,7 @@ public class RecipeCreateActivity extends AppCompatActivity {
         TabLayout tabLayout = findViewById(R.id.recipeTab);
         ViewPager2 viewPager = findViewById(R.id.recipe_info_viewpager);
         BottomNavigationView bottomNavigation = findViewById(R.id.recipe_navigation);
+        bottomNavigation.setSelectedItemId(R.id.invisible);
 
         tabLayout.addTab(tabLayout.newTab().setText("Information"));
         tabLayout.addTab(tabLayout.newTab().setText("Ingredients"));
@@ -58,35 +61,67 @@ public class RecipeCreateActivity extends AppCompatActivity {
         bottomNavigation.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
             if (itemId == R.id.recipe_back_menu) {
-                Log.v("LOL", "Back");
+                confirmExit(recipe, adapter); //TODO: Check if inputs are empty, if they are then dont save
                 finish();
                 return false;
             } else if (itemId == R.id.recipe_save_menu) {
                 Log.v("LOL", "Save");
                 return false;
-            } else if (itemId == R.id.recipe_clear_menu) {
-                Log.v("LOL", "Clear");
-                return false;
             } else if (itemId == R.id.recipe_done_menu) {
-                Toast.makeText(RecipeCreateActivity.this, "Saving...", Toast.LENGTH_SHORT).show();
-                App.getRecipeManager().create(recipe).onSuccess(createdRecipe -> {
-                    App.getRecipeManager().addImages(createdRecipe, adapter.getImageFileList()).thenAccept(result -> {
-                        RecipeCreateActivity.this.runOnUiThread(() -> {
-                            Toast.makeText(RecipeCreateActivity.this, "Saved!", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent();
-                            intent.putExtra(IntentKeys.RECIPE_SAVE, createdRecipe);
-                            setResult(Activity.RESULT_OK, intent);
-                            finish();
-                        });
-                    });
-                }).onFailed(recipeFailed -> {
-                    RecipeCreateActivity.this.runOnUiThread(() -> Toast.makeText(RecipeCreateActivity.this, recipeFailed.getMessage(), Toast.LENGTH_SHORT).show());
-                });
-                Log.v("LOL", "Done");
+                confirmPublish(recipe, adapter);
                 return true;
             }
             return false;
         });
+    }
+
+    public void confirmPublish(Recipe recipe, RecipeCreateAdapter adapter) {
+        new AlertDialog.Builder(this)
+                .setTitle("Confirm changes")
+                .setMessage("Are you sure you want to publish this recipe?")
+                .setNegativeButton("No", null)
+                .setPositiveButton("Confirm", ((dialog, which) -> {
+                    Toast.makeText(RecipeCreateActivity.this, "Saving...", Toast.LENGTH_SHORT).show();
+                    App.getRecipeManager().create(recipe).onSuccess(createdRecipe -> {
+                        App.getRecipeManager().addImages(createdRecipe, adapter.getImageFileList()).thenAccept(result -> {
+                            RecipeCreateActivity.this.runOnUiThread(() -> {
+                                Toast.makeText(RecipeCreateActivity.this, "Saved!", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent();
+                                intent.putExtra(IntentKeys.RECIPE_SAVE, createdRecipe);
+                                setResult(Activity.RESULT_OK, intent);
+                                finish();
+                            });
+                        });
+                    }).onFailed(recipeFailed -> {
+                        RecipeCreateActivity.this.runOnUiThread(() -> Toast.makeText(RecipeCreateActivity.this, recipeFailed.getMessage(), Toast.LENGTH_SHORT).show());
+                    });
+                }))
+                .show();
+    }
+
+    public void confirmExit(Recipe recipe, RecipeCreateAdapter adapter) {
+        new AlertDialog.Builder(this)
+                .setTitle("Exit without saving")
+                .setMessage("You have unsaved changes. Would you like to save the recipe as a draft?")
+                .setNegativeButton("No", null)
+                .setPositiveButton("Yes", ((dialog, which) -> {
+                    recipe.setPublic(false);
+                    Toast.makeText(RecipeCreateActivity.this, "Saving...", Toast.LENGTH_SHORT).show();
+                    App.getRecipeManager().create(recipe).onSuccess(createdRecipe -> {
+                        App.getRecipeManager().addImages(createdRecipe, adapter.getImageFileList()).thenAccept(result -> {
+                            RecipeCreateActivity.this.runOnUiThread(() -> {
+                                Toast.makeText(RecipeCreateActivity.this, "Saved!", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent();
+                                intent.putExtra(IntentKeys.RECIPE_SAVE, createdRecipe);
+                                setResult(Activity.RESULT_OK, intent);
+                                finish();
+                            });
+                        });
+                    }).onFailed(recipeFailed -> {
+                        RecipeCreateActivity.this.runOnUiThread(() -> Toast.makeText(RecipeCreateActivity.this, recipeFailed.getMessage(), Toast.LENGTH_SHORT).show());
+                    });
+                }))
+                .show();
     }
 
     @Override
