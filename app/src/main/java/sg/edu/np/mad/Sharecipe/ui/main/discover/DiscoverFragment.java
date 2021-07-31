ca876867ecfbf23aab40690b080e36b52e5683ff
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-07-31 15:43:20 +0800

    Add pull to refresh for discover.

--- app/src/main/java/sg/edu/np/mad/Sharecipe/ui/main/discover/DiscoverFragment.java
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/main/discover/DiscoverFragment.java
@@ -6,19 +6,29 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
 
 import com.facebook.shimmer.ShimmerFrameLayout;
 
 import sg.edu.np.mad.Sharecipe.R;
+import sg.edu.np.mad.Sharecipe.models.Discover;
 import sg.edu.np.mad.Sharecipe.models.DiscoverSection;
 import sg.edu.np.mad.Sharecipe.ui.App;
 import sg.edu.np.mad.Sharecipe.ui.common.SectionAdapter;
+import sg.edu.np.mad.Sharecipe.utils.FutureDataResult;
 
 public class DiscoverFragment extends Fragment {
+
+    private ShimmerFrameLayout shimmerFrameLayout;
+    private SectionAdapter adapter;
+    private SwipeRefreshLayout discoverRefresh;
+    private RecyclerView discoverRecyclerView;
 
     public DiscoverFragment() {
         // Required empty public constructor
@@ -35,17 +45,30 @@ public class DiscoverFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_discover, container, false);
 
-        RecyclerView discoverRecyclerView = view.findViewById(R.id.discoverRecyclerView);
-        ShimmerFrameLayout shimmerFrameLayout = view.findViewById(R.id.discoverShimmerLayout);
+        discoverRecyclerView = view.findViewById(R.id.discoverRecyclerView);
+        shimmerFrameLayout = view.findViewById(R.id.discoverShimmerLayout);
+        discoverRefresh = view.findViewById(R.id.discoverRefresh);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
-        SectionAdapter adapter = new SectionAdapter();
+        adapter = new SectionAdapter();
+        LayoutAnimationController controller = AnimationUtils.loadLayoutAnimation(getContext(), R.anim.layout_animation_from_bottom);
         discoverRecyclerView.setLayoutManager(layoutManager);
         discoverRecyclerView.setAdapter(adapter);
+        discoverRecyclerView.setLayoutAnimation(controller);
 
+        discoverRefresh.setOnRefreshListener(this::loadDiscover);
+
+        loadDiscover();
+
+        return view;
+    }
+
+    private void loadDiscover() {
         shimmerFrameLayout.setVisibility(View.VISIBLE);
+        discoverRecyclerView.setVisibility(View.GONE);
         shimmerFrameLayout.startShimmer();
         App.getSearchManager().discover().onSuccess(discover -> {
+            adapter.clear();
             for (DiscoverSection section : discover.getSections()) {
                 switch (section.getSizeType()) {
                     case LARGE:
@@ -57,12 +80,13 @@ public class DiscoverFragment extends Fragment {
                 }
             }
             getActivity().runOnUiThread(() -> {
+                discoverRecyclerView.setVisibility(View.VISIBLE);
                 shimmerFrameLayout.stopShimmer();
                 shimmerFrameLayout.setVisibility(View.GONE);
                 adapter.notifyDataSetChanged();
+                discoverRecyclerView.scheduleLayoutAnimation();
+                discoverRefresh.setRefreshing(false);
             });
         });
-
-        return view;
     }
 }
