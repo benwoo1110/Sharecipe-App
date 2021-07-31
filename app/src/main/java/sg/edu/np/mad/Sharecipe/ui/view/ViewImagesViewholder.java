3dc1da83927c27f820e1a761f3bf9f818456b077
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-07-31 16:54:23 +0800

    Dynamically cal recipe image size.

--- app/src/main/java/sg/edu/np/mad/Sharecipe/ui/view/ViewImagesViewholder.java
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/view/ViewImagesViewholder.java
@@ -1,7 +1,10 @@
 package sg.edu.np.mad.Sharecipe.ui.view;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -17,5 +20,13 @@ public class ViewImagesViewholder extends RecyclerView.ViewHolder {
     public ViewImagesViewholder(View itemView) {
         super(itemView);
         image = itemView.findViewById(R.id.viewImage);
+
+        // Ensure its centered properly
+        DisplayMetrics displayMetrics = Resources.getSystem().getDisplayMetrics();
+        int displayWidth = displayMetrics.widthPixels;
+        int spacing = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 56, displayMetrics);
+        int imageSpan = (displayWidth - spacing) / 2;
+        image.getLayoutParams().width = imageSpan;
+        image.getLayoutParams().height = imageSpan;
     }
 }
