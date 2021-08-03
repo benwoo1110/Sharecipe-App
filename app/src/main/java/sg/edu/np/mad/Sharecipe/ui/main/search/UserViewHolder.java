87320bbb69940f3cd765d36e9b899d17a5082148
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-08-03 17:23:58 +0800

    Add ripple effect for user view holder.

--- app/src/main/java/sg/edu/np/mad/Sharecipe/ui/main/search/UserViewHolder.java
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/main/search/UserViewHolder.java
@@ -10,12 +10,15 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.card.MaterialCardView;
+
 import sg.edu.np.mad.Sharecipe.R;
 import sg.edu.np.mad.Sharecipe.contants.IntentKeys;
 import sg.edu.np.mad.Sharecipe.ui.main.profile.UserProfileActivity;
 
 public class UserViewHolder extends RecyclerView.ViewHolder {
 
+    final MaterialCardView card;
     final TextView name;
     final TextView bio;
     final ImageView profileImage;
@@ -26,13 +29,14 @@ public class UserViewHolder extends RecyclerView.ViewHolder {
     public UserViewHolder(@NonNull View itemView) {
         super(itemView);
 
+        card = itemView.findViewById(R.id.userCard);
         name = itemView.findViewById(R.id.textViewName);
         bio = itemView.findViewById(R.id.textViewBio);
         profileImage = itemView.findViewById(R.id.userProfileImage);
         progressBar = itemView.findViewById(R.id.imageLoadProgress);
 
         Context context = itemView.getContext();
-        itemView.setOnClickListener(v -> {
+        card.setOnClickListener(v -> {
             Intent intent = new Intent(context, UserProfileActivity.class);
             intent.putExtra(IntentKeys.USER_ID, userId);
             context.startActivity(intent);
