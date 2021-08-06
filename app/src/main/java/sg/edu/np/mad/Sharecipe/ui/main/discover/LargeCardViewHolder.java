d052a462dfddd4b066f7d7fac14560e6f55b96dd
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-08-06 18:11:06 +0800

    Ripple effect for discover large card.

--- app/src/main/java/sg/edu/np/mad/Sharecipe/ui/main/discover/LargeCardViewHolder.java
+++ app/src/main/java/sg/edu/np/mad/Sharecipe/ui/main/discover/LargeCardViewHolder.java
@@ -9,12 +9,15 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.card.MaterialCardView;
+
 import sg.edu.np.mad.Sharecipe.R;
 import sg.edu.np.mad.Sharecipe.contants.IntentKeys;
 import sg.edu.np.mad.Sharecipe.ui.view.RecipeViewActivity;
 
 public class LargeCardViewHolder extends RecyclerView.ViewHolder {
 
+    final MaterialCardView card;
     final TextView title;
     final ImageView image;
     final ProgressBar progressBar;
@@ -24,11 +27,12 @@ public class LargeCardViewHolder extends RecyclerView.ViewHolder {
     public LargeCardViewHolder(@NonNull View itemView) {
         super(itemView);
 
+        card = itemView.findViewById(R.id.discoverLargeCard);
         title = itemView.findViewById(R.id.discoverLargeTitle);
         image = itemView.findViewById(R.id.discoverLargeImage);
         progressBar = itemView.findViewById(R.id.imageLoadProgress);
 
-        itemView.setOnClickListener(v -> {
+        card.setOnClickListener(v -> {
             Intent viewRecipe = new Intent(itemView.getContext(), RecipeViewActivity.class);
             viewRecipe.putExtra(IntentKeys.RECIPE_VIEW, recipeId);
             itemView.getContext().startActivity(viewRecipe);
